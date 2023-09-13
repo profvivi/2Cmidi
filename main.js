7c750e2a1d9d3c33d26b6b80af0be76778bdd786
Viviane <106716101+profvivi@users.noreply.github.com>
2023-09-13 11:01:16 -0300

    s

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+//Declarar e chamar a função, executar o som através do play
 function tocaSomPom () {
   document.querySelector('#som_tecla_pom').play();
 }
 
+//acessar o código, atribuir para tocar o som
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
