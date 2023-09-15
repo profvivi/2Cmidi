bb907b7c1513712027c6e1431cd17c1555e48d37
Viviane <106716101+profvivi@users.noreply.github.com>
2023-09-15 10:36:53 -0300

    s

--- main.js
+++ main.js
@@ -6,8 +6,3 @@
 //concede o acesso ao código, atribuir para tocar o som
 document.querySelector('.tecla_pom').onclick = tocaSomPom;*/
 
-document.querySelector('.tecla_pom').onclick = criarSomPom;
-
-function criarSomPom(){
-  document.querySelector('#som_tecla_pom').play;
-}
