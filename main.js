472ed72af4815fcbf6fde6b573de3175d64379e5
Viviane <106716101+profvivi@users.noreply.github.com>
2023-09-20 12:22:01 -0300

    s

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
+//tecla Pom
 function tocaSomPom () {
   document.querySelector('#som_tecla_pom').play();
 }
 
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
+//tecla clap
+function tocaSomClap () {
+  document.querySelector('#som_tecla_clap').play();
+}
 
+document.querySelector('.tecla_clap').onclick = tocaSomclap;
 
 
 
