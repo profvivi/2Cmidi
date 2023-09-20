8550977d88149698964f22271e2c8b565ba99bf5
Viviane <106716101+profvivi@users.noreply.github.com>
2023-09-20 12:15:09 -0300

    s

--- main.js
+++ main.js
@@ -1,12 +1,45 @@
-//Declarar e chamar a função, executar o som através do play
-/*
-querySelector para selecionar elementos
-concede o acesso ao código, atribuir para tocar o som*/
-
-
 function tocaSomPom () {
   document.querySelector('#som_tecla_pom').play();
 }
 
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Declarar e chamar a função, executar o som através do play
+/*
+querySelector para selecionar elementos
+concede o acesso ao código, atribuir para tocar o som*/
