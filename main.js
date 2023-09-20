e74a1c0047a7297c7bdfdd26201da2e771515ecc
Viviane <106716101+profvivi@users.noreply.github.com>
2023-09-20 12:12:20 -0300

    s

--- main.js
+++ main.js
@@ -2,7 +2,13 @@
 /*function tocaSomPom () {
   document.querySelector('#som_tecla_pom').play();
 }
-//querySelector para selecionar elementos
-//concede o acesso ao código, atribuir para tocar o som
-document.querySelector('.tecla_pom').onclick = tocaSomPom;*/
+querySelector para selecionar elementos
+concede o acesso ao código, atribuir para tocar o som*/
+
+
+function tocaSomPom () {
+  document.querySelector('#som_tecla_pom').play();
+}
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
