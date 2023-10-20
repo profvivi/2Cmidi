378d3128da4280a52f5618f9e1f9fb422c72f719
Viviane <106716101+profvivi@users.noreply.github.com>
2023-10-20 01:28:05 -0300

    s

--- main.js
+++ main.js
@@ -77,18 +77,18 @@ function tocaSomPom(){
   document.querySelector('#som_tecla_pom').play();
   }
 
-  function tocaSomClap(){
-      document.querySelector('#som_tecla_clap').play();
-      }
-      
-      function tocaSomTim(){
-          document.querySelector('#som_tecla_tim').play();
-          }
+function tocaSomClap(){
+  document.querySelector('#som_tecla_clap').play();
+}
 
-          const listaDeTeclas = document.querySelectorAll('.tecla');
-          listaDeTeclas[0].onclick = tocaSomPom;
-          listaDeTeclas[1].onclick = tocaSomClap;
-          listaDeTeclas[2].onclick = tocaSomTim;
+function tocaSomTim(){
+document.querySelector('#som_tecla_tim').play();
+}
+
+  const listaDeTeclas = document.querySelectorAll('.tecla');
+  listaDeTeclas[0].onclick = tocaSomPom;
+  listaDeTeclas[1].onclick = tocaSomClap;
+  listaDeTeclas[2].onclick = tocaSomTim;
 
 
 
